a997c71f3488e54ee830a6bbb45479bd98dde7ee
Christopher Stolt <stolt45@christopher-stolts-macbook-pro-2.local>
2009-11-02 17:34:35 -0800

    adding support for utility memcached server

--- cookbooks/memcached/recipes/default.rb
+++ cookbooks/memcached/recipes/default.rb
@@ -8,7 +8,7 @@ node[:applications].each do |app_name,data|
   user = node[:users].first
 
 case node[:instance_role]
- when "app", "app_master"
+ when "app", "app_master", "Memcache"
    template "/data/#{app_name}/current/config/memcached.yml" do
      source "memcached.yml.erb"
      owner user[:username]
@@ -27,6 +27,19 @@ case node[:instance_role]
      source "memcached.erb"
      variables :memusage => 512,
                :port     => 11211
+   end
+
+   remote_file "/etc/monit.d/memcached.monitrc" do
+     owner "root"
+     group "root"
+     mode 0755
+     source "memcached.monitrc"
+     action :create
+   end
+
+   execute "restart monit" do
+     command "pkill monit && telinit q"
+     action :run
    end
  end
 end
